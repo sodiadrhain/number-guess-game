da76708a992c509dff94b8048011992d0e0288ee
sodiadrhain <adesojiawobajo@gmail.com>
2020-04-22 11:36:53 +0100

    Updates

---
+++ precache-manifest.e18d6ceba66dc404c3dbd827d9a925ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10489dc7522131abb3d13f8efeac798a",
+    "url": "/number-guess-game/index.html"
+  },
+  {
+    "revision": "3c5835e58b552d38f95b",
+    "url": "/number-guess-game/static/css/main.dddb5ec4.chunk.css"
+  },
+  {
+    "revision": "07c5de2d504e7cf9a7df",
+    "url": "/number-guess-game/static/js/2.a552a814.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/number-guess-game/static/js/2.a552a814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c5835e58b552d38f95b",
+    "url": "/number-guess-game/static/js/main.7f5fbc62.chunk.js"
+  },
+  {
+    "revision": "c6d9630c7152ff07a02b",
+    "url": "/number-guess-game/static/js/runtime-main.6430a2b1.js"
+  }
+]);
